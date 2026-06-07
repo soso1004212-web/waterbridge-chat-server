ec9b2b49d4ec2a2b24af5d600ab93231877f47f6
최병천 <soso100421@gmail.com>
2026-06-07 21:56:22 +0900

    fix socket cors

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ io.on("connection", (socket) => {
 
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, "0.0.0.0", () => {
   console.log("서버 실행중", PORT);
